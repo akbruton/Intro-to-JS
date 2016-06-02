5e6fc561dc846847648765c4c2060f73a4e71062
AK <brutonak@gmail.com>
2016-06-02 11:02:53 -0500

    continued lesson

--- scripts.js
+++ scripts.js
@@ -1,4 +1,31 @@
-function sayHello() {
-    var name = 'AK';
-    alert('Hello, ' + name);
+var age = 27;
+var david = {
+    name: 'David',
+    email: 'david@gmail.com'
 };
+var tyler = {
+    name: 'Tyler',
+    email: 'tyler@gmail.com'
+};
+var haley = {
+    name: 'Haley',
+    email: 'haley@gmail.com'
+};
+var staff = [david, tyler, haley];
+
+console.log(staff);
+console.log(staff[2]);
+
+function getAgeInDogYears(humanAge) {
+    var dogAge = humanAge * 7;
+    return dogAge;
+}
+
+function sayHello(name) {
+    age = 23;
+    alert('Hello, ' + name + '. Your age is ' + getAgeInDogYears(age) + '.');
+    age = getAgeInDogYears(age);
+};
+
+sayHello('Tyler');
+console.log(age);
